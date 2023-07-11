ecd880c4e9db38620a1087a40320007ef44abe2b
Victor <vcmikhaelcp@gmail.com>
2023-07-11 17:33:20 -0300

    Produtos atraves de API

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 let produtos = []
 const endApi = 'https://whitwander.github.io/api-produtos/produtos.json'
+const elementoHtml = document.getElementById('produtos')
 getPodutos()
 
-async function getPodutos(){
+async function getPodutos() {
     const resp = await fetch(endApi)
     produtos = await resp.json()
-    console.table(produtos)
+    exibirProdutos(produtos)
+}
+
+function exibirProdutos(lista) {
+    lista.forEach(produto => {
+        elementoHtml.innerHTML += `
+        <div class="produtos__mostra">
+            <img class="produtos__foto" src="${produto.img}" alt="${produto.titulo}">
+            <div class="produtos__descrição">
+                <h3 class="produtos__titulo">${produto.titulo}</h3>
+                <p class="produtos__texto">${produto.descricao}</p>
+                <p class="produtos__preço">R$ ${produto.preco}</p>
+                <button class="produtos__link">Ver mais</button>
+            </div>
+        </div>
+        `
+    })
 }
